56b0d716aab106249c1103626debbe73778855af
Anteny Erdman <anteny30@gmail.com>
2023-03-14 10:29:30 -0700

    Added JS to verify how the form is filled

--- ProBio.js
+++ ProBio.js
@@ -8,6 +8,21 @@ function logSubmit(event) {
   const news2 = document.getElementById("contactChoice2");
 
   if (username == "") {
+    console.error("Username needs to be included.");
+  } else if (news1.checked == true && phone == "") {
+    console.error(
+      "You have to include a phone number if you want to be contacted by phone."
+    );
+  } else if (news2.checked == true && email == "") {
+    console.error(
+      "You have to include an email if you want to be contacted by email."
+    );
+  } else if (news1.checked == false && news2.checked == false) {
+    console.error("A prefered contact method needs to be selected.");
+  } else if (message == "") {
+    console.error("There needs to be a message delivered.");
+  } else if (date == "") {
+    console.error("The date needs to be entered.");
   }
 }
 
